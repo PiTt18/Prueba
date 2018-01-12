f0a99cb040f5e4ffe6dbe2219bc24598f9643bdd
pedro <pedrohernandez2003@gmail.com>
2018-01-12 20:28:45 +0100

    Código adivina el número

--- Adivina el numero.py
+++ Adivina el numero.py
@@ -1,5 +1,56 @@
 
-numero_ganador = 7
+numero_ganador = 2
+attempts = 5
+
+numero_usuario = int(input("Adivina el numero"))
+if numero_ganador == numero_usuario:
+    print("Has adiviniado!")
+else:
+    print("Inténtalo de nuevo")
+
+
+numero_ganador = 2
+attempts = 4
+
+numero_usuario = int(input("Adivina el numero"))
+if numero_ganador == numero_usuario:
+    print("Has adiviniado!")
+else:
+    print("Inténtalo de nuevo")
+
+
+numero_ganador = 2
+attempts = 3
+
+numero_usuario = int(input("Adivina el numero"))
+if numero_ganador == numero_usuario:
+    print("Has adiviniado!")
+else:
+    print("Inténtalo de nuevo")
+
+
+numero_ganador = 2
+attempts = 2
+
+numero_usuario = int(input("Adivina el numero"))
+if numero_ganador == numero_usuario:
+    print("Has adiviniado!")
+else:
+    print("Inténtalo de nuevo")
+
+
+numero_ganador = 2
+attempts = 1
+
+numero_usuario = int(input("Adivina el numero"))
+if numero_ganador == numero_usuario:
+    print("Has adiviniado!")
+else:
+    print("Inténtalo de nuevo")
+
+
+numero_ganador = 2
+attempts = 0
 
 numero_usuario = int(input("Adivina el numero"))
 if numero_ganador == numero_usuario:
